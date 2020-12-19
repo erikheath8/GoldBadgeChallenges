4bb6d098564f560a1e4bc3e118c004995be889c6
Erik Heath <erikheath8@gmail.com>
2020-12-19 16:22:23 -0500

    final project submission README Edited for Repos

---
+++ FiveGreetingsRepo/Greetings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FiveGreetingsRepository
+{
+    public enum GreetingType
+    {
+        current = 1,
+        past,
+        potential
+    }
+    public class Greetings
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public GreetingType Greeting { get; set; }
+        public string EmailString
+        {
+            get
+            {
+                if (Greeting == GreetingType.current)
+                {
+                    return "Thank you for your work with us. We appreciate your loyalty. Here's a coupon.";
+                }
+                else if (Greeting == GreetingType.past)
+                {
+                    return "It's been a long time since we've heard from you, we want you back.";
+                }
+                else if (Greeting == GreetingType.potential)
+                {
+                    return "We currently have the lowest rates on Helicopter Insurance!";
+                }
+                return null;
+            }
+        }
+
+        public Greetings() { }
+
+        public Greetings(string firstName, string lastName, GreetingType greetingType, string emailString)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Greeting = greetingType;
+        }
+
+    }
+}
---
+++ FiveGreetingsRepo/GreetingsRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FiveGreetingsRepository
+{
+    public class GreetingsRepository
+    {
+        private List<Greetings> _listOfGreetings = new List<Greetings>();
+
+        //create 
+        public void AddGreetingsToList(Greetings greeting)
+        {
+            _listOfGreetings.Add(greeting);
+        }
+
+        //read or get
+        public List<Greetings> GetGreetings()
+        {
+            return _listOfGreetings;
+        }
+
+        //delete method 
+        public bool RemoveGreetingByName(string firstName, string lastName)
+        {
+            Greetings greeting = GetGreetingsByNames(firstName, lastName);
+
+            if (greeting == null) { return false; }
+
+            int initialCount = _listOfGreetings.Count();
+            _listOfGreetings.Remove(greeting);
+
+            if (initialCount > _listOfGreetings.Count()) { return true; } else { return false; }
+
+        }
+
+        //helper method
+        public Greetings GetGreetingsByNames(string firtName, string lastName)
+        {
+            foreach(Greetings greeting in _listOfGreetings)
+            {
+                if (greeting.FirstName.ToLower() == firtName.ToLower()
+                    && greeting.LastName.ToLower() == lastName.ToLower())
+                {
+                    return greeting;
+
+                }
+            }
+            return null;
+        }
+
+    }
+}
